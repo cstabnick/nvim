83a0ef3a1246c127dd6ebd150b3fd026fbd69347
Connor Stabnick <cstabnick@gmail.com>
2025-09-10 00:09:15 -0400

    some shortcuts

--- lua/vim-opts.lua
+++ lua/vim-opts.lua
@@ -4,6 +4,12 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set ignorecase")
+vim.cmd("set smartcase")
+
 
 vim.g.mapleader = " "
-vim.keymap.set('n', '<space>h', '<cmd>noh<cr>', {desc = 'Clear Highlight'})
+vim.keymap.set('n', '<C-l>', '<cmd>noh<cr>', {desc = 'Clear Highlight'})
+vim.keymap.set('n', '<C-h>', '<cmd>set invrelativenumber<cr>', {desc = 'Toggle Relative Number'})
+vim.keymap.set('n', '<C-y>', '"*Y', {desc = 'Clear Highlight'})
