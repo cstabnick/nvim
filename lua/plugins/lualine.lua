38fb8ea79edeaa03dbcd5ba93c8a1cc96ee6ddbf
Connor Stabnick <cstabnick@gmail.com>
2026-04-06 23:28:23 -0400

    cleanup the status bar

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,21 @@
 
 return {
     "nvim-lualine/lualine.nvim",
-    config =function()
+    config = function()
         require('lualine').setup({
-        options = {
-            theme = 'dracula'
-        }})
+            options = {
+                theme = 'auto',
+                component_separators = '',
+                section_separators = '',
+            },
+            sections = {
+                lualine_a = { 'mode' },
+                lualine_b = { 'branch' },
+                lualine_c = { 'filename' },
+                lualine_x = { 'diagnostics' },
+                lualine_y = { 'filetype' },
+                lualine_z = { 'location' },
+            },
+        })
     end
 }
